8f4f21384778e1020a4dfe67d081432de4d07472
Ste Head-Rapson <stephenheadrapson@hotmail.co.uk>
2022-08-08 20:08:51 +0100

    feat(landingPage): new background and email validation

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import {
   Heading,
   Text,
   Anchor,
+  Stack,
 } from "grommet";
 
 const textColor = "light-1";
@@ -23,6 +24,11 @@ const App = () => {
   const [loading, setLoading] = useState(false);
 
   const postEmail = async (email) => {
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
     setLoading(true);
     setError("");
     const res = await fetch("http://localhost:3000/api/email", {
@@ -48,63 +54,51 @@ const App = () => {
       {(size) =>
         size !== "small" ? (
           <Box fill>
-            <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+            <Stack fill>
               <Box
+                fill
                 flex
                 align="center"
                 justify="center"
                 background="url(/background.jpg)"
+              ></Box>
+              <Box
+                fill
+                direction="row"
+                flex
+                overflow={{ horizontal: "hidden" }}
               >
-                <Box fill="horizontal">
-                  <PageHeader
-                    title={
-                      <Heading
-                        color="brand"
-                        size="large"
-                        margin="none"
-                        textAlign="center"
-                      >
-                        OudInvest
-                      </Heading>
-                    }
-                    subtitle={
-                      <Text
-                        color={textColor}
-                        size="large"
-                        weight="bold"
-                        margin="none"
-                        textAlign="center"
-                      >
-                        Halal investing for everyone, is coming.
-                      </Text>
-                    }
-                    // actions={<Button label="Edit" primary />}
-                  />
-                  <Box
-                    fill="horizontal"
-                    pad={{ left: "150px", right: "150px" }}
-                    direction="row"
-                    gap="small"
-                    align="center"
-                    justify="center"
-                  >
-                    <Box flex={{ grow: 4 }} background="light-2" round="xsmall">
-                      <TextInput
-                        placeholder="Enter email address"
-                        value={email}
-                        onChange={(e) => setEmail(e.currentTarget.value)}
-                      />
-                    </Box>
-                    <Box flex={{ grow: 1, shrink: 1 }}>
-                      <Button
-                        primary
-                        size="large"
-                        label="Get Early Access"
-                        onClick={() => postEmail(email)}
-                      />
-                    </Box>
-                  </Box>
-                  {error ? (
+                <Box
+                  flex
+                  align="center"
+                  justify="center"
+                  background="linear-gradient(0deg, rgba(0, 0, 0, 0.4) 55%, rgba(0, 0, 0, 0))"
+                >
+                  <Box fill="horizontal">
+                    <PageHeader
+                      title={
+                        <Heading
+                          color="brand"
+                          size="large"
+                          margin="none"
+                          textAlign="center"
+                        >
+                          OudInvest
+                        </Heading>
+                      }
+                      subtitle={
+                        <Text
+                          color={textColor}
+                          size="large"
+                          weight="bold"
+                          margin="none"
+                          textAlign="center"
+                        >
+                          Halal investing for everyone, is coming.
+                        </Text>
+                      }
+                      margin={{ bottom: "50px" }}
+                    />
                     <Box
                       fill="horizontal"
                       pad={{ left: "150px", right: "150px" }}
@@ -112,44 +106,76 @@ const App = () => {
                       gap="small"
                       align="center"
                       justify="center"
+                      margin={{ bottom: "50px" }}
                     >
-                      <Text
-                        color="status-error"
-                        weight="bold"
-                        textAlign="center"
+                      <Box
+                        flex={{ grow: 4 }}
+                        background="light-2"
+                        round="xsmall"
                       >
-                        {error}
+                        <TextInput
+                          placeholder="Enter email address"
+                          value={email}
+                          onChange={(e) => setEmail(e.currentTarget.value)}
+                        />
+                      </Box>
+                      <Box flex={{ grow: 1, shrink: 1 }}>
+                        <Button
+                          primary
+                          size="large"
+                          label="Get Early Access"
+                          onClick={() => postEmail(email)}
+                        />
+                      </Box>
+                    </Box>
+                    {error ? (
+                      <Box
+                        fill="horizontal"
+                        pad={{ left: "150px", right: "150px" }}
+                        direction="row"
+                        gap="small"
+                        align="center"
+                        justify="center"
+                        margin={{ bottom: "50px" }}
+                      >
+                        <Text
+                          color="status-error"
+                          weight="bold"
+                          textAlign="center"
+                        >
+                          {error}
+                        </Text>
+                      </Box>
+                    ) : null}
+                    <Box gap="small">
+                      <Text color={textColor} weight="bold" textAlign="center">
+                        OudInvest is bringing Halal Investing, to everyone
+                      </Text>
+                      <Text color={textColor} weight="bold" textAlign="center">
+                        We provide a sharia compliant commission-free digital
+                        platform to enable anybody to invest in Halal stocks,
+                        funds and commodities. Zero Fee.
+                      </Text>
+                      <Text color={textColor} weight="bold" textAlign="center">
+                        All of our verified Investment options have been
+                        screened by our Sharia advisors.{" "}
+                      </Text>
+                      <Text color={textColor} weight="bold" textAlign="center">
+                        Starting with just $1, Deposit, search, compare and
+                        invest - always Halal.
+                      </Text>
+                      <Text color={textColor} weight="bold" textAlign="center">
+                        <Anchor
+                          target="_blank"
+                          href="https://wideo.co/view/37302901659007631560?utm_source=CopyPaste&utm_medium=share&utm_campaign=sharebox&html5=true"
+                          label="How it works."
+                        />
                       </Text>
                     </Box>
-                  ) : null}
-                  <Box>
-                    <Text color={textColor} weight="bold" textAlign="center">
-                      OudInvest is bringing Halal Investing, to everyone
-                    </Text>
-                    <Text color={textColor} weight="bold" textAlign="center">
-                      We provide a sharia compliant commission-free digital
-                      platform to enable anybody to invest in Halal stocks,
-                      funds and commodities. Zero Fee.
-                    </Text>
-                    <Text color={textColor} weight="bold" textAlign="center">
-                      All of our verified Investment options have been screened
-                      by our Sharia advisors.{" "}
-                    </Text>
-                    <Text color={textColor} weight="bold" textAlign="center">
-                      Starting with just $1, Deposit, search, compare and invest
-                      - always Halal.
-                    </Text>
-                    <Text color={textColor} weight="bold" textAlign="center">
-                      <Anchor
-                        target="_blank"
-                        href="https://wideo.co/view/37302901659007631560?utm_source=CopyPaste&utm_medium=share&utm_campaign=sharebox&html5=true"
-                        label="How it works."
-                      />
-                    </Text>
                   </Box>
                 </Box>
               </Box>
-            </Box>
+            </Stack>
           </Box>
         ) : (
           <Box
